52b7d4874b7eafc7894e5f029ddc4a6e3997ec25
kjs01 <kjs01@DESKTOP-LUFSJE1>
2018-03-02 01:10:59 -0500

    Update

--- DiceApp/src/Dice.java
+++ DiceApp/src/Dice.java
@@ -1,4 +1,49 @@
 
 public class Dice {
+	public int numberOfSides;
+	public int currentSide;
 
+	/**
+	 * @param numberOfSides
+	 * @param currentSide
+	 */
+	public Dice(int numberOfSides, int currentSide) {
+		super();
+		this.numberOfSides = numberOfSides;
+		this.currentSide = currentSide;
+	}
+
+	/**
+	 * @return the numberOfSides
+	 */
+	public int getNumberOfSides() {
+		return numberOfSides;
+	}
+
+	/**
+	 * @param numberOfSides the numberOfSides to set
+	 */
+	public void setNumberOfSides(int numberOfSides) {
+		this.numberOfSides = numberOfSides;
+	}
+
+	/**
+	 * @return the currentSide
+	 */
+	public int getCurrentSide() {
+		return currentSide;
+	}
+
+	/**
+	 * @param currentSide the currentSide to set
+	 */
+	public void setCurrentSide(int currentSide) {
+		this.currentSide = currentSide;
+	}
+
+	
+
+
+	
+	
 }
--- DiceApp/src/Main.java
+++ DiceApp/src/Main.java
@@ -1,19 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		actualDice d = new actualDice();
 		d.startRolling();
-		
+
 	}
-
-
 
 }
 
 class actualDice {
-	
+	ArrayList<Dice> diceList = new ArrayList<Dice>();
+
+	Scanner sc = new Scanner(System.in);
+
 	public void startRolling() {
+
+	}
+
+	public void createDice(int userSideSelection) {
+		int sides = userSideSelection;
+
+		Dice d = new Dice(sides, 1);
+
+		diceList.add(d);
+	}
+
+	public void removeDice(Dice d) {
 		
 	}
+	
+	
+
 }
